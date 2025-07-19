87e8c8b6f0210efd807f56836e5796101311c7c1
Tushar3232 <anikdebnath916@gmail.com>
2025-07-19 20:22:06 +0600

    this is my 7th commit

--- src/Componeants/FeaturedFoods.jsx
+++ src/Componeants/FeaturedFoods.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import FeaturedFood from './featuredFood';
+import { Link } from 'react-router';
 
 const FeaturedFoods = () => {
     const  [featuredFoods, setFeaturedFoods]= useState([])
@@ -18,6 +19,15 @@ const FeaturedFoods = () => {
                 {
                     featuredFoods.map(featuredFood=> <FeaturedFood featuredFood={featuredFood} key={featuredFood._id}></FeaturedFood>)
                 }
+               
+            </div>
+                <div className="text-center my-10">
+                <Link
+                    to="/availableFoods"
+                    className="inline-block bg-amber-500 hover:bg-amber-600 text-white font-semibold py-2 px-6 rounded-lg transition-all duration-300"
+                >
+                    See All
+                </Link>
             </div>
         </div>
     );
